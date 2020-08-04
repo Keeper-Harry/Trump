b5f79dcea61864d61ac6f440b9ce960a6a0d2d9b
中井彩乃 <nakaiayano@nakaiayano-no-MacBook-Pro.local>
2020-08-04 18:42:15 +0900

    在庫エンティティを作成

--- SMF/実装/SMF/src/main/java/jp/kobe_u/cs/daikibo/SMF/controller/ZaikoController.java
+++ SMF/実装/SMF/src/main/java/jp/kobe_u/cs/daikibo/SMF/controller/ZaikoController.java
@@ -1,11 +1,20 @@
 package jp.kobe_u.cs.daikibo.SMF.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jp.kobe_u.cs.daikibo.SMF.entity.Zaiko;
+import jp.kobe_u.cs.daikibo.SMF.service.ZaikoService;
+
 @Controller
 public class ZaikoController {
+    @Autowired
+    ZaikoService zs;
+
     @GetMapping("/")
     String showIndex(){
         return "index";
@@ -13,8 +22,8 @@ public class ZaikoController {
 
     @GetMapping("/manage")
     String showlist(Model model){
-        // List<Tsubuyaki> list = ts.getAllTsubuyaki(); //全つぶやきを取得
-        // model.addAttribute("tsubuyakiList", list);   //モデル属性にリストをセット
+        List<Zaiko> list = zs.getAllZaiko(); // 在庫一覧を取得
+        model.addAttribute("zaikoList", list);   //モデル属性にリストをセット
         model.addAttribute("zaikoForm", new ZaikoForm());  //空フォームをセット
         return "zaiko";
     }
