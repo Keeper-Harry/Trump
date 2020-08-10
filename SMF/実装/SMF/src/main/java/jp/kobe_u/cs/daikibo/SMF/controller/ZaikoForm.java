b5a653dd651127988b44c8c1235eb629b7d2c9f2
中井彩乃 <nakaiayano@nakaiayano-no-MacBook-Pro.local>
2020-08-10 10:05:53 +0900

    Food エンティティを追加

--- SMF/実装/SMF/src/main/java/jp/kobe_u/cs/daikibo/SMF/controller/ZaikoForm.java
+++ SMF/実装/SMF/src/main/java/jp/kobe_u/cs/daikibo/SMF/controller/ZaikoForm.java
@@ -2,10 +2,13 @@ package jp.kobe_u.cs.daikibo.SMF.controller;
 
 import java.util.Date;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class ZaikoForm {
+    @NotBlank
     String name; //食材名
     String amount; //量
     Date expirationDate; //賞味期限
